a6c701933a91f841a140ca234dd49084b061fc7a
AbdAsh <a.mahmutoglu@cyberneticlabs.io>
2023-07-08 20:59:12 +0300

    Implement Default Layout

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,9 +1,19 @@
-import './default.css';
-function DefaultLayout() {
+import './default.scss';
+function DefaultLayout(props) {
   return (
-      <header>
+    <div id="default-layout">
+      <header className='header'>
           This is header
       </header>
+      <section className="body-container">
+        <div className="sidebar">
+          This is sidebar
+        </div>
+        <main className="main-body">
+          {props.body}
+        </main>
+      </section>
+    </div>
   );
 }
 
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,25 +1,16 @@
 import logo from '../assets/logo.svg';
 import './App.css';
-import DefaultLayout from '../layouts/default';
+
 
 function App() {
   return (
     <div className="App">
-      <DefaultLayout />
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It is me mario.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
