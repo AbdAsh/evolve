7338a4a3d0b2cd40f5838932f7ff2f9aa8300bed
AbdAsh <a.mahmutoglu@cyberneticlabs.io>
2023-07-09 22:34:13 +0300

    Fix default Layout

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -3,11 +3,9 @@ function DefaultLayout(props) {
   return (
     <div id="default-layout">
       <header className='header'>
-          This is header
       </header>
       <section className="body-container">
         <div className="sidebar">
-          This is sidebar
         </div>
         <main className="main-body">
           {props.body}
